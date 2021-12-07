51c6b3581c7afe65014e99a5907b6ee77a213140
nehaagrawl <nehaagrawal1305@gmail.com>
2021-12-07 07:38:06 +0530

    5 natural num sum

---
+++ Sum.java
@@ -0,0 +1,10 @@
+//Sum of two number
+class Sum{
+    public static void main(String args[])
+    {
+        int a=10, b=3, sum;
+        sum=a+b;
+        System.out.println("Sum of two number= "+sum);
+
+    }
+}
