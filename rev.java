51c0b5f00e62d8893d314d5cc6a283e11e028312
Neha <nehaagrawal1305@gmail.com>
2022-01-02 19:13:16 +0530

    reverse String

---
+++ rev.java
@@ -0,0 +1,12 @@
+// reverse string
+class rev
+{
+    public static void main(String args[])
+    {
+        String s="neha agrawal";
+        StringBuilder n=new StringBuilder(s);
+        n.reverse();
+        System.out.println("Reverse String: "+n);
+        
+    }
+}
